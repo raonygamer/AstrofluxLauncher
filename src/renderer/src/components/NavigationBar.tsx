3a1956c4658d6aaa462f3e1a2ce0e67ce4598d28
Raony Fernandes dos Reis <75029692+raonygamer@users.noreply.github.com>
2025-04-07 11:18:42 -0300

    Commit

---
+++ src/renderer/src/components/NavigationBar.tsx
@@ -0,0 +1,13 @@
+import "./NavigationBar.scss"
+
+export default function NavigationBar() {
+    return (
+        <div className="navigation-bar-root">
+            <div className="navigation-bar-items">
+                <div className="navigation-bar-item">
+                    <div className="navigation-bar-item-text">Launcher Page</div>
+                </div>
+            </div>
+        </div>
+    )
+}
